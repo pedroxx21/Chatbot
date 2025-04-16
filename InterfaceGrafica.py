36de97701a65d04fde78878e0ae825a1b3f80449
Pedro Henrique Ribeiro Lopes <lopesph06@gmail.com>
2025-04-16 14:51:59 -0300

    mds

---
+++ InterfaceGrafica.py
@@ -0,0 +1,14 @@
+import ProjetoChatbot as pc 
+from tkinter import *
+
+main_window = Tk()
+
+main_window.title("Chatbot Dona Maria")
+main_window.geometry("500x700")
+
+main_window.grid()
+
+main_window.mainlopp()
+
+
+
